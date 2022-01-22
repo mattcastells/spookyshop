64b8db1e06d5d3d814f5cf760eeb5a8919457e18
Matias <gulincastellsmatias@gmail.com>
2022-01-22 14:36:27 -0300

    Add text to contact

--- src/routes/contacto.jsx
+++ src/routes/contacto.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./contact.sass"
 
-
 const Contacto = () => {
     return (
         <div>
             <div className="contact-container">
-                <h3 className="contact">Podes contactarnos desde el mas alla con tu medium o bruja de confianza</h3>
+                <h3 className="contact">¡Podes contactarnos desde el mas alla con tu medium o bruja de confianza!</h3>
+                <h3 h3 className="contact">0-800-666-boladecristal</h3>
             </div>
         </div>
     )
