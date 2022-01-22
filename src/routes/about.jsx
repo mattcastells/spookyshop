353f99360240d1b8eaeb40f6f6d513677147ba82
Matias <gulincastellsmatias@gmail.com>
2022-01-22 14:37:02 -0300

    Add new class to about

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import "./about.sass";
-import "./zombieAbout.png";
 
 const About = () => {
     return (
         <div className="container">
-            <h4>¡Hola! Esta pagina es el proyecto final del curso de React de CODERHOUSE. La misma fue realizada con la finalidad de explorar la utilizacion del framework y sus capacidades. 
+            <h4 className="container-text">¡Hola! Esta pagina es el proyecto final del curso de React de CODERHOUSE. La misma fue realizada con la finalidad de explorar la utilizacion del framework y sus capacidades. 
                 La pagina tiene un tono comico, siendo el primer e-commerce creado para "zombis" jaja.
                 Pagina realizada por Matias Gulin</h4>
-            <img src={require('./zombieAbout.png')} alt="zombie"></img>
         </div>
     )
 }
